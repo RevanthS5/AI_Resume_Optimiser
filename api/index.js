e9ae60d15f73ba62d3e75d5d7d08bcc672b4de26
Revanth Srinivasa <revanthsrinivasa20@gmail.com>
2025-08-17 19:27:54 +0100

    Vercel config fixing

--- api/index.js
+++ api/index.js
@@ -3,3 +3,6 @@ const app = require('../backend/app');
 
 // Export the Express app as a Vercel serverless function
 module.exports = app;
+
+// Add this line for Vercel serverless functions
+module.exports.default = app;
